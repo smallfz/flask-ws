ca1a65aaa7f1db7a669a99e7d3671b2b183b6d27
smallfz <small.fz@gmail.com>
2016-04-03 18:28:43 +0800

    make it a package

---
+++ setup.py
@@ -0,0 +1,17 @@
+#-*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+
+packages = (
+    'flaskws',
+    )
+
+setup(name='flask-ws', 
+      version='0.0.1.0', 
+      packages=packages, 
+      description='Websocket for flask.',
+      author='smallfz', 
+      author_email='small.fz@gmail.com')
+
+
